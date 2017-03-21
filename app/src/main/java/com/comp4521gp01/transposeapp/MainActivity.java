41800da5e94c2066ea74969ed6d43e51bfb7fa89
waimancheung <wymancheung95@gmail.com>
2017-03-21 15:40:25 +0800

    Add test for google vision api

--- app/src/main/java/com/comp4521gp01/transposeapp/MainActivity.java
+++ app/src/main/java/com/comp4521gp01/transposeapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.comp4521gp01.transposeapp;
 
 import java.io.File;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import android.os.Environment;
@@ -14,16 +16,23 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ArrayAdapter;
 
 import com.github.clans.fab.FloatingActionButton;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ArrayAdapter<String> listAdapter;
     private String[] list = {"鉛筆","原子筆","鋼筆","毛筆","彩色筆"};
+
+    ImageView imageView;
+    Button btnProcess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +65,33 @@ public class MainActivity extends AppCompatActivity {
         mFab_Cam.setOnClickListener(clickListener);
         mFab_Gal.setOnClickListener(clickListener);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+        btnProcess = (Button) findViewById(R.id.button);
 
+        final Bitmap bitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.test
+        );
+        //imageView.setImageBitmap(bitmap);
+
+        btnProcess.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                TextRecognizer textRecognizer =  new TextRecognizer.Builder(getApplicationContext()).build();
+                if(!textRecognizer.isOperational()){
+                    Log.e("ERROR", "Detector dependencies are not yet availble");
+                }else{
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> items = textRecognizer.detect(frame);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for(int i=0; i<items.size();i++){
+                        TextBlock item = items.valueAt(i);
+                        stringBuilder.append(item.getValue());
+                        stringBuilder.append("\n");
+                    }
+                    debug.setText(stringBuilder.toString());
+                }
+            }
+        });
 
     }
 
